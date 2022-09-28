ef6c97ab7016f5f9992529db7e4b59b118329403
Abel Mendoza <abelxmendoza@gmail.com>
2022-09-27 21:46:14 -0700

    first push

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,6 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x04D5c5bd83bcB80ddFF84901f2f113Fdd045A182";
+
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
